84facf4c9a812d989f9f386e5eea636b1610baf2
Trond Albinussen <trond.albinussen@gmail.com>
2021-01-11 22:14:22 +0100

    Sass and react bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import './App.scss';
+import { Button } from 'react-bootstrap';
+
+const click = () => {
+  console.log('click');
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Button onClick={click} variant='primary'>
+        Primary
+      </Button>{' '}
+      <Button variant='secondary'>Secondary</Button> <Button variant='success'>Success</Button>{' '}
+      <Button variant='warning'>Warning</Button> <Button variant='danger'>Danger</Button>{' '}
+      <Button variant='info'>Info</Button> <Button variant='light'>Light</Button>{' '}
+      <Button variant='dark'>Dark</Button> <Button variant='link'>Link</Button>
     </div>
   );
 }
